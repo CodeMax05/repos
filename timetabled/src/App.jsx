84a052e1c8613e8d209102aba9b4a0fc07992af3
David Loja <d4vd.005@gmail.com>
2026-03-25 21:54:03 -0400

    Co-authored-by: Avanish Arif <a.arif013895@gmail.com> Co-authored-by: Hayden-C-Reynolds <Hayden-C-Reynolds@users.noreply.github.com> Co-authored-by: Vuyo <lion191@users.noreply.github.com>

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Calendar from './components/Calender';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Itinerary for 7 Days in Korea</h1>
+      <h3>Welcome to Korea, David! Check out this calender to get to know the city and see </h3>
+      <Calendar />
+      
+    </div>
+  )
+}
+
+export default App
