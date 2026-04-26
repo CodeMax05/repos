f1a49fd4b68bc4744fbe971018c99e2060b98de1
David Loja <d4vd.005@gmail.com>
2026-04-26 19:17:34 -0400

    Fixed img oversize in home page in class cards

--- 4vibes/src/components/Navbar.jsx
+++ 4vibes/src/components/Navbar.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Navbar = () =>{
     return (
         <div>
-         
+            <ul>
+                <li>Home</li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
         </div>
     );
 }
--- crewmate/src/pages/Home.jsx
+++ crewmate/src/pages/Home.jsx
@@ -21,28 +21,29 @@ function Home() {
       <div style={{ marginTop: '3rem', textAlign: 'left' }}>
         <h2>Dragon Classes</h2>
         <div className="dragons-grid" style={{ marginTop: '1rem' }}>
+
           <div className="dragon-card strike">
             <span className="dragon-class-badge strike">Strike Class</span>
-            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0d4a8dbf-84c1-46f3-b740-e10dd863774f/d5hwjkz-38ba9559-465e-462a-8bd5-ab8c6eba16d5.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi8wZDRhOGRiZi04NGMxLTQ2ZjMtYjc0MC1lMTBkZDg2Mzc3NGYvZDVod2prei0zOGJhOTU1OS00NjVlLTQ2MmEtOGJkNS1hYjhjNmViYTE2ZDUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.n9urSbet2mLdv09VHitr3bPTlz_qyavZEz54ZXwmACU" alt="strike class" />
+            <img id="classImg" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0d4a8dbf-84c1-46f3-b740-e10dd863774f/d5hwjkz-38ba9559-465e-462a-8bd5-ab8c6eba16d5.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi8wZDRhOGRiZi04NGMxLTQ2ZjMtYjc0MC1lMTBkZDg2Mzc3NGYvZDVod2prei0zOGJhOTU1OS00NjVlLTQ2MmEtOGJkNS1hYjhjNmViYTE2ZDUucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.n9urSbet2mLdv09VHitr3bPTlz_qyavZEz54ZXwmACU" alt="strike class" />
             <h3>Lightning Fast</h3>
             <p>Strike Class dragons are known for their incredible speed and blazing firepower. The Night Fury is the pride of this class.</p>
           </div>
           <div className="dragon-card sharp">
             <span className="dragon-class-badge sharp">Sharp Class</span>
-            <img src="https://www.clipartmax.com/png/full/250-2505514_sharp-class-symbol-by-xelku9-how-to-train-your-dragon-dragon-class.png" alt="sharp class" />
+            <img id="classImg" src="https://www.clipartmax.com/png/full/250-2505514_sharp-class-symbol-by-xelku9-how-to-train-your-dragon-dragon-class.png" alt="sharp class" />
             
             <h3>Deadly Precision</h3>
             <p>Sharp Class dragons possess deadly precision and can slice through anything with their razor-sharp spines and tails.</p>
           </div>
           <div className="dragon-card boulder">
             <span className="dragon-class-badge boulder">Boulder Class</span>
-            <img src="https://www.nicepng.com/png/full/204-2042408_boulder-class-symbol-by-xelku9-d5hwini-train-your.png" alt="Boulder class" />
+            <img id="classImg" src="https://www.nicepng.com/png/full/204-2042408_boulder-class-symbol-by-xelku9-d5hwini-train-your.png" alt="Boulder class" />
             <h3>Tough as Stone</h3>
             <p>Boulder Class dragons are built tough with rock-hard skin. They can blast dangerous projectiles and withstand heavy attacks.</p>
           </div>
           <div className="dragon-card stoker">
             <span className="dragon-class-badge stoker">Stoker Class</span>
-            <img src="https://httyddragons.weebly.com/uploads/4/2/4/5/42457497/1709771.png?250" alt="Stocker class" />
+            <img id="classImg" src="https://httyddragons.weebly.com/uploads/4/2/4/5/42457497/1709771.png?250" alt="Stocker class" />
             <h3>Master of Fire</h3>
             <p>Stoker Class dragons are fiery powerhouses with the hottest flames. They can set themselves on fire when enraged.</p>
           </div>
