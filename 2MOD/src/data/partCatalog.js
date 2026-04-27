dd88ab1430bea1c68d8e81f415e3300197bb5f4a
David Loja <d4vd.005@gmail.com>
2026-04-27 02:45:10 -0400

    catalog for the images in the categorization of the guitar parts

---
+++ 2MOD/src/data/partCatalog.js
@@ -0,0 +1,39 @@
+import nut from "../assets/guitarNut.png";
+
+export const PART_CATALOG = [
+  {
+    name: "Body",
+    description: "Body shapes, wood types, finishes, and contours",
+    image:
+      "https://www.fender.com/cdn-cgi/image/format=auto,resize=height=auto,width=850/https://www.fmicassets.com/Damroot/eCommPNG/10001/0998003700_fen_acc_frt_1_rr.png",
+  },
+  {
+    name: "Electronics",
+    description: "Pickups, pots, switches, and wiring configurations",
+    image:
+      "https://www.fender.com/cdn-cgi/image/format=auto,resize=height=auto,width=850/https://www.fmicassets.com/Damroot/eCommPNG/10001/0992344506_merch_frt_001_rr.png",
+  },
+  {
+    name: "Bridge",
+    description: "Fixed bridges, tremolos, and hardware upgrades",
+    image:
+      "https://www.fender.com/cdn-cgi/image/format=auto,resize=height=auto,width=850/https://www.fmicassets.com/Damroot/eCommPNG/10001/0054619000_merch_frt_001_nr.png",
+  },
+  {
+    name: "Neck",
+    description: "Neck profiles, fretboards, and scale lengths",
+    image:
+      "https://www.fender.com/cdn-cgi/image/format=auto,resize=height=auto,width=850/https://www.fmicassets.com/Damroot/eCommPNG/10109/0994230921_fen_nck_frt_1_rr.png",
+  },
+  {
+    name: "Tuners",
+    description: "Tuning machines, locking tuners, and stability",
+    image:
+      "https://www.fender.com/cdn-cgi/image/format=auto,resize=height=auto,width=850/https://www.fmicassets.com/Damroot/eCommPNG/10001/0990818000_fen_acc_dtl_1_nr.png",
+  },
+  {
+    name: "Nut",
+    description: "Nut materials, slot widths, and string spacing",
+    image: nut,
+  },
+];
